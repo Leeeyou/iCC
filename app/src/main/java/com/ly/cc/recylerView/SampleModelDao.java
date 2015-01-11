d6854991fb6783ef92a9bb5d0c6dc8e5e1d7e093
KongbeiForCoding <kongbeily@qq.com>
2015-01-11 17:15:06 +0800

    完成Android5.0 新控件RecyclerView的功能和效果

---
+++ app/src/main/java/com/ly/cc/recylerView/SampleModelDao.java
@@ -0,0 +1,16 @@
+package com.ly.cc.recylerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kongbei on 2015/1/11.
+ */
+public class SampleModelDao {
+    public static ArrayList<SampleModel> getSampleData(int size) {
+        ArrayList<SampleModel> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(new SampleModel("新建项目 <" + i + "> "));
+        }
+        return list;
+    }
+}
