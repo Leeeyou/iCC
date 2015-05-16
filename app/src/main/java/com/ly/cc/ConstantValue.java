fb3c97c7fcd313567a768deb4c881d604468794f
LeeeYou <lzf.515>
2015-05-16 10:26:45 +0800

    JDLKFJLAS

--- app/src/main/java/com/ly/cc/ConstantValue.java
+++ app/src/main/java/com/ly/cc/ConstantValue.java
@@ -3,5 +3,6 @@ package com.ly.cc;
 
 public interface ConstantValue {
 	/**********************************************/
+	/***/
 	String[] appStores = { "com.qihoo.appstore", "com.tencent.android.qqdownloader", "com.wandoujia.phoenix2", "com.taobao.appcenter", "com.baidu.appsearch", "com.xiaomi.market", "com.huawei.appmarket", "com.yulong.android.coolmart", "zte.com.market", "com.suning.market", "com.gionee.aora.market", "com.infinit.wostore.ui", "com.lenovo.leos.appstore", "com.mumayi.market.ui", "com.skymobi.appstore", "com.hiapk.marketpho", "cn.com.zol.market", "cn.goapk.market", "com.sogou.appmall", "com.sogou.androidtool", "com.sec.android.app.samsungapps", "com.android.vending", "com.dragon.android.pandaspace", "com.mappn.gfan", "com.zmapp" };
 }
