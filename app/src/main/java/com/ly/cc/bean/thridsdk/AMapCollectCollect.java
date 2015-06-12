2ac20e6a02f5917c5b47395274a6572586750a03
LeeeYou <leeeyou@163.com>
2015-06-12 23:21:56 +0800

    GPS定位

---
+++ app/src/main/java/com/ly/cc/bean/thridsdk/AMapCollectCollect.java
@@ -0,0 +1,8 @@
+package com.ly.cc.bean.thridsdk;
+
+/**
+ * Created by xzzz on 2015/5/14.
+ */
+public class AMapCollectCollect {
+    public static final String[] items = {" ▶ 网络定位", " ▶ GPS定位", " ▶ 当前天气", " ▶ 未来天气"};
+}
