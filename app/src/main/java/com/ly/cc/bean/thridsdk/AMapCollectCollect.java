d694dca8c623dcf1f3a9c1f924fa8962fa8f2771
LeeeYou <leeeyou@163.com>
2015-06-13 23:55:18 +0800

    更新定位MainActivity样式

--- app/src/main/java/com/ly/cc/bean/thridsdk/AMapCollectCollect.java
+++ app/src/main/java/com/ly/cc/bean/thridsdk/AMapCollectCollect.java
@@ -4,5 +4,5 @@ package com.ly.cc.bean.thridsdk;
  * Created by xzzz on 2015/5/14.
  */
 public class AMapCollectCollect {
-    public static final String[] items = {" ▶ 网络定位", " ▶ GPS定位", " ▶ 当前天气", " ▶ 未来天气"};
+    public static final String[] items = {"网络定位", "GPS定位", "当前天气", "未来天气"};
 }
